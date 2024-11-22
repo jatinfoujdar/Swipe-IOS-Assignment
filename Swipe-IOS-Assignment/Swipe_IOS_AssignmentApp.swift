5625f7e0526b1e0ec1ed697aa582c3c7ace579db
Jatin Foujdar <jatin321001@gmail.com>
2024-11-22 21:59:57 +0530

    Initial Commit

---
+++ Swipe-IOS-Assignment/Swipe_IOS_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swipe_IOS_AssignmentApp.swift
+//  Swipe-IOS-Assignment
+//
+//  Created by jatin foujdar on 22/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Swipe_IOS_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
