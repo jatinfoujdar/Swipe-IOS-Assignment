3aa9589271b4ed6e268e94af26bd67ccca74375d
Jatin Foujdar <jatin321001@gmail.com>
2024-11-22 22:13:34 +0530

    product model commit

---
+++ Swipe-IOS-Assignment/Models/ProductModel.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+
+struct Product: Identifiable,Decodable{
+    
+    var id = UUID()
+    let image: String
+    let price: Double
+    let productName: String
+    let productType: String
+    let tax: Double
+    
+    
+    enum CodingKeys: String, CodingKey {
+    case image
+    case price
+    case productName = "product_name"
+    case productType = "product_type"
+    case tax
+        }
+    
+}
